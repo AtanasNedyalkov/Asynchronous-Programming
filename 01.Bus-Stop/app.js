78cc9aa5c8793af51bd6f98c73348ddc080a927c
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-11-03 13:38:43 +0200

    Add project files.

---
+++ 01.Bus-Stop/app.js
@@ -0,0 +1,27 @@
+async function getInfo() {
+    console.log("TODO...");
+    const stopInfoElement = document.getElementById("stopId");
+    const stopId = stopInfoElement.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+    const stopNameElement = document.getElementById("stopName");
+    const busList = document.getElementById("buses");
+
+    busList.innerHTML = "";
+    stopId.value = "";
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        stopNameElement.textContent = data.name;
+        Object.entries(data.buses).forEach(([busNumber, timeArrival])=>{
+            const li = document.createElement("li")
+            li.textContent = `Bus ${busNumber} arrives in ${timeArrival} minutes`
+            busList.appendChild(li);
+        })
+    }
+    catch(e){
+        stopNameElement.textContent = "Error";
+
+    }
+
+}
